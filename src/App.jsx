7f50c391a6baa7410af5d8bdb2bf596ad018057a
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-08 10:04:59 +0530

    basic setup done; BrowserRouter, Routes and Route added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-      cleaned
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
