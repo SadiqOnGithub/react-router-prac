fce846ee6e5a5e28a2087981ac2f69c4f6253874
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-08 13:13:04 +0530

    adding NavLink for active link option

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function NavBar() {
+
+  const navLinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? 'bold' : 'normal',
+      textDecoration: isActive ? 'underline' : 'none'
+    };
+  };
+
   return (
     <>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about" >About</Link>
+        <NavLink style={navLinkStyles} to="/">Home</NavLink>
+        <NavLink style={navLinkStyles} to="/about" >About</NavLink>
       </nav>
     </>
   );
