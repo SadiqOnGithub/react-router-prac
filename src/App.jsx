30f878d84f85eece36b19f56587433de7dc170f6
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-09 15:23:32 +0530

    NoMatch route or 404 or page not found handled

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import NavBar from "./components/NavBar";
 import OrderSummary from "./components/OrderSummary";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/order-summary" element={<OrderSummary /> } />
+        <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
